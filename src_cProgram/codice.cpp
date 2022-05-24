f188cbfe63e4814f79b05703abf0847ed128c67d
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2022-05-24 18:50:33 +0200

    Iniziata nuova scrittura del codice

--- src_cProgram/codice.cpp
+++ src_cProgram/codice.cpp
@@ -3,11 +3,11 @@
 #include "dependencies/include/libpq-fe.h"
 
 //Parametri database
-# define PG_HOST "localhost" // Indirizzo Host
-# define PG_USER "amassare" // Nome utente
-# define PG_DB "progetto_amassare_fzontaro" //Nome del database
-# define PG_PASS "%zNqTm:0wF4x" //Password
-# define PG_PORT 8080
+#define PG_HOST "localhost" // Indirizzo Host
+#define PG_USER "amassare" // Nome utente
+#define PG_DB "progetto_amassare_fzontaro" //Nome del database
+#define PG_PASS "%zNqTm:0wF4x" //Password
+#define PG_PORT 8080
 
 //Query
 #define QUERY_1 "SELECT Ev.Specialita, MAX(Os.Nome) AS Presentatore, MAX(Az.Nome) AS Sponsor, MAX(Pa.Nazionalita) AS Vincitori FROM Evento Ev JOIN Ospite Os ON Ev.Presentatore=Os.Codice JOIN Azienda Az ON Ev.Sponsor=Az.Codice JOIN Partecipante Pa ON Pa.CF=Ev.Vincitore GROUP BY Ev.Specialita"
@@ -17,177 +17,88 @@
 #define QUERY_5 "DROP VIEW IF EXISTS Vendite; CREATE VIEW Vendite AS SELECT Prod.Codice, Prod.Nome, Prod.Produttore, Prod.Prezzo, Cont.Quantita FROM Prodotto Prod JOIN Contenuto Cont ON Prod.Codice=Cont.Prodotto; SELECT St.Nome AS Stand, Pers.Nome AS Proprietario FROM (SELECT Pe.Codice, COALESCE(Nome, 'Membro Staff') Nome FROM Personale Pe LEFT JOIN (SELECT Codice, Nome FROM Azienda UNION SELECT Codice, Nome FROM Ospite) AS Speciali ON Pe.Codice=Speciali.Codice) AS Pers JOIN Stand St ON Pers.Codice=St.Proprietario JOIN (SELECT DISTINCT Va.Stand FROM Vendita Va WHERE NOT EXISTS (SELECT DISTINCT Codice FROM Vendite Ve WHERE Va.Prodotto=Ve.Codice)) AS NonVe ON NonVe.Stand=St.Codice"
 #define QUERY_6 "SELECT Pa.Nome, COALESCE(Stand, 0) Stand, COALESCE(Eventi, 0) Eventi FROM Padiglione Pa LEFT JOIN (	SELECT Pa.Nome, COUNT(Ev.Padiglione) AS Eventi FROM Padiglione Pa JOIN Evento Ev ON Pa.Nome=Ev.Padiglione GROUP BY Pa.Nome) AS PaEv ON Pa.Nome=PaEv.Nome LEFT JOIN (	SELECT Pa.Nome,  COUNT(St.Padiglione) AS Stand FROM Padiglione Pa  JOIN Stand St ON Pa.Nome=St.Padiglione GROUP BY Pa.Nome ) AS PaSt ON Pa.Nome=PaSt.Nome ORDER BY Pa.Nome"
 
-void do_exit(PGconn *conn)
-{
-    PQfinish(conn);
-    exit(1);
+using namespace std;
+
+void checkResults(PGresult* res, const PGconn* conn){
+    if(PQresultStatus(res) != PGRES_TUPLES_OK){
+        cout<<"Risultati inconsistenti!"<<PQerrorMessage(conn);
+        PQclear(res);
+        exit(1);
+    }
 }
 
-class Dbable{
-    public:
-        void checkResults ( PGresult * res , const PGconn * conn );
-        virtual ~Dbable(){};
-};
+void stampaResults(PGresult* res){
 
-class Imbarcazione: public Dbable{
-public:
-    Imbarcazione();
-    void ormeggia(int molo);
-    void disormeggia();
-    int getLung() const;
-    int getLargh() const;
-    int getProf() const;
-private:
-    int lung,largh,prof;
-};
+    int tuple = PQntuples(res);
+    int campi = PQnfields(res);
+    cout<<endl;
 
-class Marina:public Dbable{
-public: 
-    void printFreeDocks(PGconn* conn);
-    int getFreeDock(PGconn* conn,const Imbarcazione& imb);
-};
+    for(int i=0; i<campi; ++i){
+        cout << PQfname(res, i) << "\t\t";
+    }
+    cout << endl<<endl;
 
+    for(int i=0; i<tuple; ++i){
+        for(int j=0; j<campi; ++j){
+            cout<<PQgetvalue(res, i, j)<<"\t\t";
+        }
+        cout<<endl;
+    }
 
-class Table{
-    friend std::ostream& operator<<(std::ostream& os,const Table& table);
-    public:
-        Table(PGresult* res);
-        int getRowsCount() const;
-        int getColsCount() const;
-        
-    private:
-        PGresult* res;
-};
+    PQclear(res);
+}
 
-std::ostream& operator<< (std::ostream& os,const Table& table);
+void execQuery(PGconn* conn, const char* q){
 
-int main()
-{
+    PGresult *res = PQexec(conn, q);
+
+    checkResults(res, conn);
+    stampaResults(res);
+
+}
+
+int main(int argc, char **argv){
+    cout<<"Start"<<endl<<endl;
 
     char conninfo[250];
-    sprintf(conninfo , "user=%s password=%s dbname=%s host=%s port=%d" , PG_USER , PG_PASS , PG_DB , PG_HOST , PG_PORT );
+    sprintf(conninfo, "user=%s password=%s dbname=%s hostaddr=%s port=%d", PG_USER, PG_PASS, PG_DB, PG_HOST, PG_PORT);
 
+    PGconn * conn = PQconnectdb (conninfo);
 
-
-    PGconn *conn = PQconnectdb(conninfo); //Connessione al database
-    if ( PQstatus ( conn ) != CONNECTION_OK ) {
-        std::cout << " Errore di connessione " << PQerrorMessage(conn);
-        PQfinish(conn) ;
-        exit (1) ;
-    }
-    else {
-        std::cout << " Connessione avvenuta correttamente \n"; 
+    if(PQstatus(conn) != CONNECTION_OK){
+        cout<<"Errore di connessione "<<PQerrorMessage(conn)<<endl;
+        PQfinish(conn);
+        exit(1);
     }
 
+    const char* query[]={QUERY_1, QUERY_2, QUERY_3, QUERY_4, QUERY_5, QUERY_6};
 
-    std::cout<< "------------------------------\n";
-    std::cout<< "Scegli un'opzione dal menù \n";
-    std::cout<< "1. Ormeggia Imbarcazione\n";
-    std::cout<< "2. Disormeggia Imbarcazione\n";
-    std::cout<< "------------------------------\n";
-
-    int i = 0;
-
-    std::cin >> i;
-
-    Marina marina;
-    switch (i)
-    {
-    case(1):
-        std::cout << "Dammi l'id della barca da ormeggiare";
-        marina.getFreeDock(conn,Imbarcazione());
-        //Fai cose relative all'inserimento barca
-        break;
-    case(2):
-        std::cout << "Scegli la barca da disormeggiare\n";
-        break;    
-    default:
-        //Di che c'è stato un errore e rimostra il menù
-        break;
+    for(int q=0; q<6; q++){
+        cout<<"Query "<<q+1<<endl;
+        execQuery(conn, query[q]);
+        cout<<endl;
     }
 
-    //Pulizia finale
-    PQfinish(conn);
-}
-
-//Implementazioni ---------------------------------------------
-
-void Marina::printFreeDocks(PGconn* conn){
-    PGresult *res = PQexec(conn, "SELECT * FROM molo where occupato = false");
-    checkResults(res,conn);
-
-    Table t(res);
-    std::cout << t;
-}
-
-void Dbable::checkResults (PGresult * res , const PGconn * conn ) {
-    if ( PQresultStatus(res) != PGRES_TUPLES_OK) {
-        std::cout << " Risultati inconsistenti ! " << PQerrorMessage(conn) << std::endl ;
-            PQclear(res) ;
-            exit (1) ;
-        }
-}
-
-Table::Table(PGresult* re) : res(re){}
-
-
-int Table::getRowsCount() const{return PQntuples(res);}
-int Table::getColsCount() const{return PQnfields(res);}
-
-std::ostream& operator<<(std::ostream& os,const Table& table){
-    for(int i = 0; i < table.getColsCount(); ++i){
-            os << PQfname(table.res,i) << "\t\t";
-        }
-
-        os << std::endl;
-
-        for(int i = 0; i < table.getRowsCount(); ++i){
-            for(int j = 0; j < table.getColsCount(); ++j){
-                os << PQgetvalue(table.res,i,j) << "\t\t";
-            }
-            os << std::endl;
-        }
-    return os;
-}
-
-int Marina::getFreeDock(PGconn* conn,const Imbarcazione& imb){
-    PGresult* ressss = PQprepare(conn,
-                              "mario",
-                              "SELECT id FROM molo"
-                              "WHERE occupato = false"
-                              "AND lunghezza > 1"
-                              "AND larghezza > 1"
-                              "AND profondita > $1::int"
-                              "LIMIT 1",
-                              3,
-                              NULL);
-
-    std::cout << ressss << "\n";
-    std::string padiglione;
-    std::cin>>padiglione;
+    string parStand = "SELECT St.Nome, Pers.Nome, St.Funzione FROM Stand St JOIN (SELECT Pe.Codice, COALESCE(Nome, 'Membro Staff') Nome FROM Personale Pe LEFT JOIN (SELECT Codice, Nome FROM Azienda UNION SELECT Codice, Nome FROM Ospite) AS Speciali ON Pe.Codice=Speciali.Codice) AS Pers ON St.Proprietario=Pers.Codice WHERE Padiglione=$1::varchar";
+    PGresult *stmtST = PQprepare(conn, "stand_parametro", parStand.c_str(), 1, NULL);
+    string parEvento = "SELECT Orario, Specialita, Ev.Nome, (Pa.Nome, Pa.Cognome) AS Vincitore, Premio, Os.Nome AS Presentatore, Az.Nome AS Sponsor FROM Evento Ev JOIN Ospite Os ON Ev.Presentatore=Os.Codice JOIN Azienda Az ON Ev.Sponsor=Az.Codice JOIN Partecipante Pa ON Ev.Vincitore=Pa.CF WHERE Padiglione=$1::varchar";
+    PGresult *stmtEV = PQprepare(conn, "eventi_parametro", parEvento.c_str(), 1, NULL);
+    string padiglione;
+    cout<<"Inserire Padiglione di cui cercare le informazioni"<<endl;
+    cin>>padiglione;
     const char *parameter = padiglione.c_str();
 
     PGresult *res;
-    res=PQexecPrepared(conn, "mario", 1, &parameter, NULL, 0, 0);
-    checkResults(res,conn);
+    res=PQexecPrepared(conn, "stand_parametro", 1, &parameter, NULL, 0, 0);
+    checkResults(res, conn);
+    cout<<"Stand"<<endl;
+    stampaResults(res);
+    res=PQexecPrepared(conn, "eventi_parametro", 1, &parameter, NULL, 0, 0);
+    checkResults(res, conn);
+    cout<<endl<<"Eventi"<<endl;
+    stampaResults(res);
 
 
-    Table t(res);
-    std::cout << t;
-    //std::cout<<PQgetvalue(res, 0, 0);
-
+    cout<<endl<<"Finish"<<endl;
     return 0;
 }
-
-
-Imbarcazione::Imbarcazione() {}
-
-void Imbarcazione::ormeggia(int molo) {}
-
-void Imbarcazione::disormeggia() {}
-
-int Imbarcazione::getLung() const {return lung;}
-
-int Imbarcazione::getLargh() const {return largh;}
-
-int Imbarcazione::getProf() const {return prof;}
