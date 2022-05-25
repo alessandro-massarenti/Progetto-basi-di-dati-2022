e417f904f404bbe4c72f6f260d4d713c21616348
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2022-05-25 13:31:33 +0200

    close #26

--- useful_stuff.sql
+++ useful_stuff.sql
@@ -41,3 +41,32 @@ WHERE molo.id in (select molo from o) ;
 select * from imbarcazione;
 
 select * from libero_ora;
+
+
+
+
+---Cose per sistemare il db
+
+select from sosta,imbarcazione where imbarcazione.id = sosta.imbarcazione;
+
+
+
+with o as(select molo from molo_occupato)
+
+UPDATE molo
+SET occupato = true
+WHERE molo.id in (select molo from o) ;
+
+--Trovare un cliente che non ha fatto soste
+
+with s as (select imbarcazione from sosta)
+
+select * from imbarcazione where id in (select s.imbarcazione from s);
+
+with i as (select cliente from imbarcazione)
+select * from cliente where persona not in (select i.cliente from i);
+
+
+with c as (select max(id) as max from cliente)
+
+SELECT setval('cliente_id_seq', (select c.max from c), true);
