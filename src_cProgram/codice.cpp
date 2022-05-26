c0d6f8c2455e2374e2fdbcac03a0e4f11f0c8b09
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2022-05-26 15:32:10 +0200

    Sistemato programma e aggiunto risultato alla query 5

--- src_cProgram/codice.cpp
+++ src_cProgram/codice.cpp
@@ -1,104 +1,177 @@
 // g++ codice.cpp -L depencies\lib -lpq -o codice
 #include <iostream>
+#include <string>
+#include <cstdio>
 #include "dependencies/include/libpq-fe.h"
 
 //Parametri database
-#define PG_HOST "localhost" // Indirizzo Host
+#define PG_HOST "127.0.0.1" // Indirizzo Host
 #define PG_USER "amassare" // Nome utente
 #define PG_DB "progetto_amassare_fzontaro" //Nome del database
 #define PG_PASS "%zNqTm:0wF4x" //Password
-#define PG_PORT 8080
+#define PG_PORT "8080"
 
 //Query
-#define QUERY_1 "SELECT Ev.Specialita, MAX(Os.Nome) AS Presentatore, MAX(Az.Nome) AS Sponsor, MAX(Pa.Nazionalita) AS Vincitori FROM Evento Ev JOIN Ospite Os ON Ev.Presentatore=Os.Codice JOIN Azienda Az ON Ev.Sponsor=Az.Codice JOIN Partecipante Pa ON Pa.CF=Ev.Vincitore GROUP BY Ev.Specialita"
-#define QUERY_2 "SELECT Au.Casa, TRUNC(AVG(Pa.Eta)) AS EtaMedia FROM Partecipante Pa JOIN Automobile Au ON Pa.CF=Au.Proprietario GROUP BY Au.Casa ORDER BY AVG(Pa.Eta)"
-#define QUERY_3 "SELECT Acquirente, SUM(Totale) AS TotaleAcquisti FROM Acquisto Ac, (SELECT Proprietario FROM Automobile Au JOIN Partecipazione Pa ON Au.Targa=Pa.Automobile GROUP BY Proprietario HAVING COUNT(Evento)>1) AS Partecipanti WHERE Ac.Acquirente=Partecipanti.Proprietario GROUP BY Acquirente"
-#define QUERY_4 "DROP VIEW IF EXISTS Epoca; CREATE VIEW Epoca AS SELECT Targa, DATE_PART('year', AGE(CURRENT_DATE, Anno)) AS Eta, CASE WHEN DATE_PART('year', AGE(CURRENT_DATE, Anno))>=25 THEN 'Storica' WHEN DATE_PART('year', AGE(CURRENT_DATE, Anno))<25 THEN 'Attuale' END Epoca FROM Automobile; SELECT Nazionalita, COUNT(Epoca) FROM Partecipante Pa JOIN Automobile Au ON Pa.CF=Au.Proprietario JOIN Epoca E ON Au.Targa=E.Targa WHERE Epoca='Storica' GROUP BY Nazionalita ORDER BY COUNT(Epoca) DESC"
-#define QUERY_5 "DROP VIEW IF EXISTS Vendite; CREATE VIEW Vendite AS SELECT Prod.Codice, Prod.Nome, Prod.Produttore, Prod.Prezzo, Cont.Quantita FROM Prodotto Prod JOIN Contenuto Cont ON Prod.Codice=Cont.Prodotto; SELECT St.Nome AS Stand, Pers.Nome AS Proprietario FROM (SELECT Pe.Codice, COALESCE(Nome, 'Membro Staff') Nome FROM Personale Pe LEFT JOIN (SELECT Codice, Nome FROM Azienda UNION SELECT Codice, Nome FROM Ospite) AS Speciali ON Pe.Codice=Speciali.Codice) AS Pers JOIN Stand St ON Pers.Codice=St.Proprietario JOIN (SELECT DISTINCT Va.Stand FROM Vendita Va WHERE NOT EXISTS (SELECT DISTINCT Codice FROM Vendite Ve WHERE Va.Prodotto=Ve.Codice)) AS NonVe ON NonVe.Stand=St.Codice"
-#define QUERY_6 "SELECT Pa.Nome, COALESCE(Stand, 0) Stand, COALESCE(Eventi, 0) Eventi FROM Padiglione Pa LEFT JOIN (	SELECT Pa.Nome, COUNT(Ev.Padiglione) AS Eventi FROM Padiglione Pa JOIN Evento Ev ON Pa.Nome=Ev.Padiglione GROUP BY Pa.Nome) AS PaEv ON Pa.Nome=PaEv.Nome LEFT JOIN (	SELECT Pa.Nome,  COUNT(St.Padiglione) AS Stand FROM Padiglione Pa  JOIN Stand St ON Pa.Nome=St.Padiglione GROUP BY Pa.Nome ) AS PaSt ON Pa.Nome=PaSt.Nome ORDER BY Pa.Nome"
+#define QUERY_1 ""
+#define QUERY_2 ""
+#define QUERY_3 ""
+#define QUERY_4 ""
+#define QUERY_5 "with intestazione_fattura as(select fattura.*, p.nome, p.cognome from fattura join cliente c on c.persona = fattura.cliente join persona p on c.persona = p.cf where cliente = 'GLLGNN81A54G224W'), spese as( select sosta.fattura,i.cliente,'sosta' as tipo,ROUND((tstzrange_subdiff(sosta.partenza,sosta.arrivo)/86400.0 * m.prezzogiorno)::numeric,2)  as prezzo from sosta join imbarcazione i on i.id = sosta.imbarcazione join molo m on sosta.molo = m.id where partenza != 'infinity' union all select consumo.fattura,consumo.cliente,'consumo' as tipo, ROUND((consumo.quantita * a2.prezzounitario)::numeric,2) as prezzo from consumo join fornitura f on consumo.allacciamento = f.allacciamento join allacciamento a2 on consumo.allacciamento = a2.nome) select distinct id, scadenza, pagato, nome, cognome, intestazione_fattura.cliente, tipo, prezzo    from spese, intestazione_fattura where spese.fattura = intestazione_fattura.id;"
 
-using namespace std;
+using std::cout;
+using std::endl;
+using std::string;
+using std::cin;
 
-void checkResults(PGresult* res, const PGconn* conn){
-    if(PQresultStatus(res) != PGRES_TUPLES_OK){
-        cout<<"Risultati inconsistenti!"<<PQerrorMessage(conn);
-        PQclear(res);
-        exit(1);
-    }
-}
+PGconn* connect(const char* host, const char* user, const char* db, const char* pass, const char* port) {
+    char conninfo[256];
+    sprintf(conninfo, "user=%s password=%s dbname=\'%s\' hostaddr=%s port=%s",
+            user, pass, db, host, port);
 
-void stampaResults(PGresult* res){
+    PGconn* conn = PQconnectdb(conninfo);
 
-    int tuple = PQntuples(res);
-    int campi = PQnfields(res);
-    cout<<endl;
-
-    for(int i=0; i<campi; ++i){
-        cout << PQfname(res, i) << "\t\t";
-    }
-    cout << endl<<endl;
-
-    for(int i=0; i<tuple; ++i){
-        for(int j=0; j<campi; ++j){
-            cout<<PQgetvalue(res, i, j)<<"\t\t";
-        }
-        cout<<endl;
-    }
-
-    PQclear(res);
-}
-
-void execQuery(PGconn* conn, const char* q){
-
-    PGresult *res = PQexec(conn, q);
-
-    checkResults(res, conn);
-    stampaResults(res);
-
-}
-
-int main(int argc, char **argv){
-    cout<<"Start"<<endl<<endl;
-
-    char conninfo[250];
-    sprintf(conninfo, "user=%s password=%s dbname=%s hostaddr=%s port=%d", PG_USER, PG_PASS, PG_DB, PG_HOST, PG_PORT);
-
-    PGconn * conn = PQconnectdb (conninfo);
-
-    if(PQstatus(conn) != CONNECTION_OK){
-        cout<<"Errore di connessione "<<PQerrorMessage(conn)<<endl;
+    if (PQstatus(conn) != CONNECTION_OK) {
+        std::cerr << "Errore di connessione" << endl << PQerrorMessage(conn);
         PQfinish(conn);
         exit(1);
     }
 
-    const char* query[]={QUERY_1, QUERY_2, QUERY_3, QUERY_4, QUERY_5, QUERY_6};
-
-    for(int q=0; q<6; q++){
-        cout<<"Query "<<q+1<<endl;
-        execQuery(conn, query[q]);
-        cout<<endl;
+    return conn;
+}
+PGresult* execute(PGconn* conn, const char* query) {
+    PGresult* res = PQexec(conn, query);
+    if (PQresultStatus(res) != PGRES_TUPLES_OK) {
+        cout << " Risultati inconsistenti!" << PQerrorMessage(conn) << endl;
+        PQclear(res);
+        exit(1);
     }
 
-    string parStand = "SELECT St.Nome, Pers.Nome, St.Funzione FROM Stand St JOIN (SELECT Pe.Codice, COALESCE(Nome, 'Membro Staff') Nome FROM Personale Pe LEFT JOIN (SELECT Codice, Nome FROM Azienda UNION SELECT Codice, Nome FROM Ospite) AS Speciali ON Pe.Codice=Speciali.Codice) AS Pers ON St.Proprietario=Pers.Codice WHERE Padiglione=$1::varchar";
-    PGresult *stmtST = PQprepare(conn, "stand_parametro", parStand.c_str(), 1, NULL);
-    string parEvento = "SELECT Orario, Specialita, Ev.Nome, (Pa.Nome, Pa.Cognome) AS Vincitore, Premio, Os.Nome AS Presentatore, Az.Nome AS Sponsor FROM Evento Ev JOIN Ospite Os ON Ev.Presentatore=Os.Codice JOIN Azienda Az ON Ev.Sponsor=Az.Codice JOIN Partecipante Pa ON Ev.Vincitore=Pa.CF WHERE Padiglione=$1::varchar";
-    PGresult *stmtEV = PQprepare(conn, "eventi_parametro", parEvento.c_str(), 1, NULL);
-    string padiglione;
-    cout<<"Inserire Padiglione di cui cercare le informazioni"<<endl;
-    cin>>padiglione;
-    const char *parameter = padiglione.c_str();
+    return res;
+}
 
-    PGresult *res;
-    res=PQexecPrepared(conn, "stand_parametro", 1, &parameter, NULL, 0, 0);
-    checkResults(res, conn);
-    cout<<"Stand"<<endl;
-    stampaResults(res);
-    res=PQexecPrepared(conn, "eventi_parametro", 1, &parameter, NULL, 0, 0);
-    checkResults(res, conn);
-    cout<<endl<<"Eventi"<<endl;
-    stampaResults(res);
+void printLine(int campi, int* maxChar) {
+    for (int j = 0; j < campi; ++j) {
+        cout << '+';
+        for (int k = 0; k < maxChar[j] + 2; ++k)
+            cout << '-';
+    }
+    cout << "+\n";
+}
+void printQuery(PGresult* res) {
+    // Preparazione dati
+    const int tuple = PQntuples(res), campi = PQnfields(res);
+    string v[tuple + 1][campi];
+
+    for (int i = 0; i < campi; ++i) {
+        string s = PQfname(res, i);
+        v[0][i] = s;
+    }
+    for (int i = 0; i < tuple; ++i)
+        for (int j = 0; j < campi; ++j) {
+            if (string(PQgetvalue(res, i, j)) == "t" || string(PQgetvalue(res, i, j)) == "f")
+                if (string(PQgetvalue(res, i, j)) == "t")
+                    v[i + 1][j] = "si";
+                else
+                    v[i + 1][j] = "no";
+            else
+                v[i + 1][j] = PQgetvalue(res, i, j);
+        }
+
+    int maxChar[campi];
+    for (int i = 0; i < campi; ++i)
+        maxChar[i] = 0;
+
+    for (int i = 0; i < campi; ++i) {
+        for (int j = 0; j < tuple + 1; ++j) {
+            int size = v[j][i].size();
+            maxChar[i] = size > maxChar[i] ? size : maxChar[i];
+        }
+    }
+
+    // Stampa effettiva delle tuple
+    printLine(campi, maxChar);
+    for (int j = 0; j < campi; ++j) {
+        cout << "| ";
+        cout << v[0][j];
+        for (int k = 0; k < maxChar[j] - v[0][j].size() + 1; ++k)
+            cout << ' ';
+        if (j == campi - 1)
+            cout << "|";
+    }
+    cout << endl;
+    printLine(campi, maxChar);
+
+    for (int i = 1; i < tuple + 1; ++i) {
+        for (int j = 0; j < campi; ++j) {
+            cout << "| ";
+            cout << v[i][j];
+            for (int k = 0; k < maxChar[j] - v[i][j].size() + 1; ++k)
+                cout << ' ';
+            if (j == campi - 1)
+                cout << "|";
+        }
+        cout << endl;
+    }
+    printLine(campi, maxChar);
+}
+
+char* chooseParam(PGconn* conn, const char* query, const char* table) {
+    PGresult* res = execute(conn, query);
+    printQuery(res);
+
+    const int tuple = PQntuples(res), campi = PQnfields(res);
+    int val;
+    cout << "Inserisci il numero del " << table << " scelto: ";
+    cin >> val;
+    while (val <= 0 || val > tuple) {
+        cout << "Valore non valido\n";
+        cout << "Inserisci il numero del " << table << " scelto: ";
+        cin >> val;
+    }
+    return PQgetvalue(res, val - 1, 0);
+}
+
+int main(int argc, char** argv) {
+    PGconn* conn = connect(PG_HOST, PG_USER, PG_DB, PG_PASS, PG_PORT);
+
+    const char* query[6] = {QUERY_1,
+                            QUERY_2,
+                            QUERY_3,
+                            QUERY_4,
+                            QUERY_5
+    };
+
+    while (true) {
+        cout << endl;
+
+        cout << "1. Consumi del marina nel mese indicato\n";
+        cout << "2. Clienti con più di 2 di prenotazioni che iniziano nel quinquiennio 2020->2025\n";
+        cout << "3. Conteggio soste di ogni imbarcazione\n";
+        cout << "4. Controllo dei posti disponibili per una certa imbarcazione\n";
+        cout << "5. Mostra la fattura di un cliente\n";
 
 
-    cout<<endl<<"Finish"<<endl;
-    return 0;
+        cout << "Query da eseguire (0 per uscire): ";
+        int q = 0;
+        cin >> q;
+        while (q < 0 || q > 6) {
+            cout << "Le query vanno da 1 a 6...\n";
+            cout << "Query da eseguire (0 per uscire): ";
+            cin >> q;
+        }
+        if (q == 0) break;
+        char queryTemp[5000];
+
+        int i = 0;
+        switch (q) {
+            default:
+                printQuery(execute(conn, query[q - 1]));
+                break;
+        }
+        string a = "";
+        cout << "Press any key and press enter..";
+        cin >> a;
+    }
+
+    PQfinish(conn);
 }
