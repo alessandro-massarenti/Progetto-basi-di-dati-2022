d9c54b66402e737361a5d865ea00999a6fcb42f4
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2022-05-27 13:03:15 +0200

    Nuova versione del codice e migliorata la leggibilità delle chiavi primarie

--- src_cProgram/codice.cpp
+++ src_cProgram/codice.cpp
@@ -18,13 +18,9 @@
 #define QUERY_4 "select distinct molo.id as id_molo,molo.prezzogiorno,molo.profonditaminima,molo.larghezza,molo.lunghezza, molo.occupato from molo,imbarcazione where occupato=false and molo.larghezza > imbarcazione.larghezza and molo.profonditaminima > imbarcazione.pescaggio and molo.lunghezza > imbarcazione.loa and imbarcazione.mmsi = '8836340' order by prezzogiorno limit 5;"
 #define QUERY_5 "with intestazione_fattura as(select fattura.*, p.nome, p.cognome from fattura join cliente c on c.persona = fattura.cliente join persona p on c.persona = p.cf where cliente = 'GLLGNN81A54G224W'), spese as( select sosta.fattura,i.cliente,'sosta' as tipo,ROUND((tstzrange_subdiff(sosta.partenza,sosta.arrivo)/86400.0 * m.prezzogiorno)::numeric,2)  as prezzo from sosta join imbarcazione i on i.id = sosta.imbarcazione join molo m on sosta.molo = m.id where partenza != 'infinity' union all select consumo.fattura,consumo.cliente,'consumo' as tipo, ROUND((consumo.quantita * a2.prezzounitario)::numeric,2) as prezzo from consumo join fornitura f on consumo.allacciamento = f.allacciamento join allacciamento a2 on consumo.allacciamento = a2.nome) select distinct id, scadenza, pagato, nome, cognome, intestazione_fattura.cliente, tipo, prezzo    from spese, intestazione_fattura where spese.fattura = intestazione_fattura.id;"
 
-using std::cout;
-using std::endl;
-using std::string;
-using std::cin;
 
-
-class ResultTable {
+//Classe che rappresenta una tabella di risultato
+class ResultTable{
 private:
     PGresult *res;
     int righe,colonne;
@@ -34,33 +30,80 @@ public:
     int getRighe() const{return righe;}
     int getColonne() const{return colonne;}
 private:
-    static void printLine(int campi, const size_t *maxChar) {
-        for (int j = 0; j < campi; ++j) {
-            cout << '+';
-            for (int k = 0; k < maxChar[j] + 2; ++k)
-                cout << '-';
-        }
-        cout << "+\n";
-    }
-    static void traduciBool(string & val) {
-        if (val == "t") {
-            val = "si";
-            return;
-        }
-        if (val == "f") {
-            val = "no";
-            return;
-        }
-    }
+    static void printSeparator(int campi, const size_t *maxChar);
+    static void traduciBool(std::string & val);
 };
+
+//Dichiarazioni metodi
+PGconn *connect(const char *host, const char *user, const char *db, const char *pass, const char *port);
+PGresult *execute(PGconn *conn, const char *query);
+void printMenu();
+//--------------------
+
+//Main
+int main(int argc, char **argv) {
+    PGconn *conn = connect(PG_HOST, PG_USER, PG_DB, PG_PASS, PG_PORT);
+
+    const char *query[6] = {QUERY_1,
+                            QUERY_2,
+                            QUERY_3,
+                            QUERY_4,
+                            QUERY_5
+    };
+
+    int q = -1;
+    while (q != 0) {
+        printMenu();
+        std::cin >> q;
+        if(q < 0 || q > 5){
+            std::cout << "Le query vanno da 1 a 5...\n";
+            std::cout << "Query da eseguire (0 per uscire): ";
+        }if(q!=0){
+            switch (q) {
+
+                default:{
+                    ResultTable rt(execute(conn, query[q - 1]));
+                    std::cout << rt;
+                    break;
+                }
+            }
+            std::cout << "1 torna al menù principale, 0 esce\n";
+            std::cin >> q;
+            if(q != 1 || q != 1) std::cout << "si ho mentito andava bene qualsiasi numero diverso da 0\n";
+        }
+    }
+
+    PQfinish(conn);
+}
+
+//Implementazioni
+//-------------------------------
+void ResultTable::printSeparator(int campi, const size_t *maxChar) {
+    for (int j = 0; j < campi; ++j) {
+        std::cout << '+';
+        for (int k = 0; k < maxChar[j] + 2; ++k)
+            std::cout << '-';
+    }
+    std::cout << "+\n";
+}
+void ResultTable::traduciBool(std::string & val) {
+    if (val == "t") {
+        val = "si";
+        return;
+    }
+    if (val == "f") {
+        val = "no";
+        return;
+    }
+}
 
 std::ostream &operator<<(std::ostream &os, const ResultTable &t) {
     // Preparazione dati
 
-    string v[t.getRighe() + 1][t.getColonne()];
+    std::string v[t.getRighe() + 1][t.getColonne()];
 
     for (int i = 0; i < t.getColonne(); ++i) {
-        string s = PQfname(t.res, i);
+        std::string s = PQfname(t.res, i);
         v[0][i] = s;
     }
     //Vengono tradotti i bool
@@ -76,43 +119,42 @@ std::ostream &operator<<(std::ostream &os, const ResultTable &t) {
         for (int j = 0; j < t.getRighe() + 1; ++j) {
             maxChar[i] = v[j][i].size() > maxChar[i] ? v[j][i].size() : maxChar[i];
         }
-        cout << maxChar[i] << endl;
+        std::cout << maxChar[i] << std::endl;
     }
 
     // Stampa effettiva delle tuple
-    ResultTable::printLine(t.getColonne(), maxChar);
+    ResultTable::printSeparator(t.getColonne(), maxChar);
 
     //Stampa intestazione
     for (int j = 0; j < t.getColonne(); ++j) {
-        cout << "| ";
-        cout << v[0][j];
+        std::cout << "| ";
+        std::cout << v[0][j];
         for (int k = 0; k < maxChar[j] - v[0][j].size() + 1; ++k)
-            cout << ' ';
+            std::cout << ' ';
         if (j == t.getColonne() - 1)
-            cout << "|";
+            std::cout << "|";
     }
-    cout << endl;
+    std::cout << std::endl;
 
 
-    ResultTable::printLine(t.getColonne(), maxChar);
+    ResultTable::printSeparator(t.getColonne(), maxChar);
 
     for (int i = 1; i < t.getRighe() + 1; ++i) {
         for (int j = 0; j < t.getColonne(); ++j) {
-            cout << "| ";
+            std::cout << "| ";
 
-            cout << v[i][j];
+            std::cout << v[i][j];
 
             for (int k = 0; k < (maxChar[j] - v[i][j].size()) + 1; ++k)
-                cout << ' ';
+                std::cout << ' ';
         }
-        cout << "|";
-        cout << endl;
+        std::cout << "|";
+        std::cout << std::endl;
     }
 
-    ResultTable::printLine(t.getColonne(), maxChar);
+    ResultTable::printSeparator(t.getColonne(), maxChar);
     return os;
 }
-
 
 PGconn *connect(const char *host, const char *user, const char *db, const char *pass, const char *port) {
     char conninfo[256];
@@ -122,7 +164,7 @@ PGconn *connect(const char *host, const char *user, const char *db, const char *
     PGconn *conn = PQconnectdb(conninfo);
 
     if (PQstatus(conn) != CONNECTION_OK) {
-        std::cerr << "Errore di connessione" << endl << PQerrorMessage(conn);
+        std::cerr << "Errore di connessione" << std::endl << PQerrorMessage(conn);
         PQfinish(conn);
         exit(1);
     }
@@ -133,56 +175,22 @@ PGconn *connect(const char *host, const char *user, const char *db, const char *
 PGresult *execute(PGconn *conn, const char *query) {
     PGresult *res = PQexec(conn, query);
     if (PQresultStatus(res) != PGRES_TUPLES_OK) {
-        cout << " Risultati inconsistenti!" << PQerrorMessage(conn) << endl;
+        std::cout << " Risultati inconsistenti!" << PQerrorMessage(conn) << std::endl;
         PQclear(res);
         exit(1);
     }
     return res;
 }
 
-int main(int argc, char **argv) {
-    PGconn *conn = connect(PG_HOST, PG_USER, PG_DB, PG_PASS, PG_PORT);
+void printMenu() {
+    std::cout << std::endl;
 
-    const char *query[6] = {QUERY_1,
-                            QUERY_2,
-                            QUERY_3,
-                            QUERY_4,
-                            QUERY_5
-    };
-
-    while (true) {
-        cout << endl;
-
-        cout << "1. Consumi del marina nel mese indicato\n";
-        cout << "2. Clienti con più di 2 di prenotazioni che iniziano nel quinquiennio 2020->2025\n";
-        cout << "3. Conteggio soste di ogni imbarcazione\n";
-        cout << "4. Controllo dei posti disponibili per una certa imbarcazione\n";
-        cout << "5. Mostra la fattura di un cliente\n";
+    std::cout << "1. Consumi del marina nel mese indicato\n";
+    std::cout << "2. Clienti con più di 2 di prenotazioni che iniziano nel quinquiennio 2020->2025\n";
+    std::cout << "3. Conteggio soste di ogni imbarcazione\n";
+    std::cout << "4. Controllo dei posti disponibili per una certa imbarcazione\n";
+    std::cout << "5. Mostra la fattura di un cliente\n";
 
 
-        cout << "Query da eseguire (0 per uscire): ";
-        int q = 0;
-        cin >> q;
-        while (q < 0 || q > 6) {
-            cout << "Le query vanno da 1 a 5...\n";
-            cout << "Query da eseguire (0 per uscire): ";
-            cin >> q;
-        }
-        if (q == 0) break;
-        char queryTemp[5000];
-
-        int i = 0;
-        switch (q) {
-            default:{
-                ResultTable rt(execute(conn, query[q - 1]));
-                cout << rt;
-                break;
-            }
-        }
-        string a = "";
-        cout << "Press any key and press enter..";
-        cin >> a;
-    }
-
-    PQfinish(conn);
+    std::cout << "scegli cosa eseguire (0 per uscire): ";
 }
